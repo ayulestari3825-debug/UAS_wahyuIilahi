07ccf3994ca47e8d2b6c3770ae88099ad9437cda
ayulestari3825-debug <ayulestari3825@gmail.com>
2026-07-28 15:42:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function pesan(){
+
+alert("Terima kasih telah mengunjungi UMKM Moci Pekanbaru");
+
+}
+
+function wa(){
+
+window.open(
+"https://wa.me/6281234567890?text=Halo%20saya%20ingin%20memesan%20Moci",
+"_blank"
+);
+
+}
